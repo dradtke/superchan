ac308242110c8c1408c668e8ad8d2842427505c2
Damien Radtke <dradtke@channeliq.com>
2014-12-11 10:35:38 -0600

    Add documentation.

---
+++ target/doc/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['superchan'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='superchan/tcp/struct.TcpReceiver.html' title='superchan::tcp::TcpReceiver'>TcpReceiver</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['superchan'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='superchan/enum.ReceiverError.html' title='superchan::ReceiverError'>ReceiverError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
